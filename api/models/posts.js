9ce2cd589935cf973cb2d802d30aa40b257c420b
MdTasib <ohidulalam442@gmail.com>
2021-06-15 15:37:59 +0600

    create newpost

--- api/models/posts.js
+++ api/models/posts.js
@@ -12,15 +12,22 @@ class Post {
 
     }
 
-    add() {
+    add(newPost) {
         // Add new post
-
+        const currentPosts = this.readData();
+        currentPosts.unshift(newPost);
+        this.storeData(currentPosts);
     }
 
     readData() {
         let rawData = fs.readFileSync(PATH);
         let posts = JSON.parse(rawData);
         return posts;
+    }
+
+    storeData(rawData) {
+        let data = JSON.stringify(rawData);
+        fs.writeFileSync(PATH, data);
     }
 }
 
